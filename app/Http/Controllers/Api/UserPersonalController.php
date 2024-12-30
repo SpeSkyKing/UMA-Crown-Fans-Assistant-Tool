8de7d4a40d780502866816c85b5f5ad9bbc7fcc3
SpeSkyKing <hongmingjitian272@gmail.com>
2024-12-30 15:50:17 +0900

    🎨 Userのログインログアウトなどの認証機能の追加

--- app/Http/Controllers/Api/UserPersonalController.php
+++ app/Http/Controllers/Api/UserPersonalController.php
@@ -5,17 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Models\UserPersonal;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 
 class UserPersonalController extends Controller
 {
     public function regist(Request $request)
     {
-        try{
+        try {
             $validated = $request->validate([
-                'user_id' => ['required', 'string', 'unique:user_table'],
+                'user_name' => ['required', 'string', 'unique:user_table'],
                 'password' => ['required', 'string', 'min:7'],
                 'email' => ['required', 'string', 'email', 'max:255', 'unique:user_table'],
                 'phone' => ['required', 'string', 'max:255'],
@@ -25,7 +25,7 @@ class UserPersonalController extends Controller
                 'country' => ['required', 'string', 'max:255'],
                 'role' => ['required','in:0,1'],
             ]);
-        }catch (\Illuminate\Validation\ValidationException $e) {
+        } catch (\Illuminate\Validation\ValidationException $e) {
             Log::error('バリデーションエラー:', $e->errors());
             return response()->json(['errors' => $e->errors()], 400);
         }
@@ -36,9 +36,10 @@ class UserPersonalController extends Controller
             $userImage = str_replace('public/', '', $userImage);
         }
 
-        try{
+        try {
             $user = new UserPersonal();
-            $user->user_id = $validated['user_id'];
+            $user->user_id = UserPersonal::max('user_id') + 1 ?: 1;
+            $user->user_name = $validated['user_name'];
             $user->password = Hash::make($validated['password']);
             $user->email = $validated['email'];
             $user->phone_number = $validated['phone'];
@@ -50,9 +51,9 @@ class UserPersonalController extends Controller
             $user->state = true;
             $user->role = $validated['role'];
             $user->save();
-        }catch (\Illuminate\Validation\ValidationException $e) {
-            Log::error('ユーザー登録エラー:', $e->errors());
-            return response()->json(['errors' => $e->errors()], 400);
+        } catch (\Exception $e) {
+            Log::error('ユーザー登録エラー:', $e->getMessage());
+            return response()->json(['error' => 'ユーザー登録エラー'], 500);
         }
 
         return response()->json([
@@ -60,22 +61,57 @@ class UserPersonalController extends Controller
             'user' => $user,
         ], 201);
     }
-    public function login(Request $request) {
+
+    public function login(Request $request) 
+    {
         $request->validate([
-            'user_id' => 'required|string',
+            'user_name' => 'required|string',
             'password' => 'required|string',
         ]);
 
-
-        $user = UserPersonal::where('user_id', $request->user_id)->first();
+        $user = UserPersonal::where('user_name', $request->user_name)->first();
 
         if ($user && Hash::check($request->password, $user->password)) {
 
-            Auth::login($user);
+            $token = $user->createToken('UMA-Crown-Fans-Assistant-Tool')->plainTextToken;
 
-            return response()->json(['message' => 'ログイン成功'], 200);
+            return response()->json([
+                'message' => 'ログイン成功',
+                'token' => $token,
+            ], 200);
         }
 
         return response()->json(['message' => '認証失敗'], 401);
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::user()->tokens->each(function ($token) {
+            $token->delete();
+        });
+
+        return response()->json(['message' => 'ログアウトしました']);
+    }
+
+    public function getUserData(Request $request)
+    {
+        $user = Auth::user()->only([
+            'user_name',
+            'email',
+            'phone_number',
+            'user_image',
+            'birthday',
+            'gender',
+            'location',
+            'country',
+            'state',
+            'role'
+        ]);;
+
+        if (!$user) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return response()->json(['data' => $user]);
     }
 }
