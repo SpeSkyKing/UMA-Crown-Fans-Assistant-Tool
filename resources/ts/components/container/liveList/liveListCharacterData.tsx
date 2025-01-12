a75710f1cdde67702ace3f81a2371ee8ed1bd9b0
SpeSkyKing <hongmingjitian272@gmail.com>
2025-01-12 11:42:29 +0900

    🐛  バグがあったため修正

--- resources/ts/components/container/liveList/liveListCharacterData.tsx
+++ resources/ts/components/container/liveList/liveListCharacterData.tsx
@@ -3,8 +3,10 @@ import {LiveListCharacterDataProps} from '../../interface/props';
 import {TdItem} from '../../common/tdItem';
 export const LiveListCharacterData : React.FC<LiveListCharacterDataProps> = ({umamusume}) => {
     return (
-        <TdItem content=
-            {umamusume.umamusume_name}
-        />
+        <tr>
+            <TdItem content=
+                {umamusume.umamusume_name}
+            />
+        </tr>
     );
 };
